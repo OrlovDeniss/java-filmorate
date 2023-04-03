8593d4fe92a022fb52091ee94fe27a876fc49a93
Орлов Денис <orlovdenis.m@yandex.ru>
2023-04-03 16:09:26 +0500

    feat add database 2

--- src/main/java/ru/yandex/practicum/filmorate/exception/FilmNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/FilmNotFoundException.java
@@ -2,12 +2,6 @@ package ru.yandex.practicum.filmorate.exception;
 
 public class FilmNotFoundException extends RuntimeException {
 
-    private static final String defaultMessage = "Фильм не найден";
-
-    public FilmNotFoundException() {
-        super(defaultMessage);
-    }
-
     public FilmNotFoundException(String message) {
         super(message);
     }
